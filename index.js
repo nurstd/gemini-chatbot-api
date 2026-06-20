cf5da7a3d4536ed4bca39849bc5488be2b2386eb
nurstd <w.nurhidayah07@gmail.com>
2026-06-20 16:06:41 +0700

    Add endpoint generate from audio & doc

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const PORT = 3000;
 app.listen(PORT, () => console.log(`Server ready on http://localhost:${PORT}`));
 
 //endpoint untuk generate text
-app.post('/generate-text', async (req, res) => {
+app.post('/generate-from-text', async (req, res) => {
     const { prompt } = req.body;
 
     try {
@@ -35,22 +35,16 @@ app.post('/generate-text', async (req, res) => {
     }
 });
 
-//endpoint untuk generate gambar
-app.post('/generate-image', upload.single('image'), async (req, res) => {
+app.post('/generate-from-image', upload.single('image'), async (req, res) => {
     const { prompt } = req.body;
-    const base64Image = req.file.buffer.toString("base64");
+    const base64Image = req.file.buffer.toString('base64');
 
     try {
         const response = await ai.models.generateContent({
             model: GEMINI_MODEL,
             contents: [
-                { text: prompt, type: "text" },
-                {
-                    inlineData: {
-                        mimeType: req.file.mimetype,
-                        data: base64Image
-                    }
-                }
+                { text: prompt, type: 'text' },
+                { inlineData: { data: base64Image, mimeType: req.file.mimetype } }
             ]
         });
         res.status(200).json({ result: response.text });
@@ -58,4 +52,42 @@ app.post('/generate-image', upload.single('image'), async (req, res) => {
         console.log(e);
         res.status(500).json({ message: e.message });
     }
-})
+});
+
+app.post('/generate-from-document', upload.single('document'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Document = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt ?? 'Tolong buatkan ringkasan dari dokumen ini.', type: 'text' },
+                { inlineData: { data: base64Document, mimeType: req.file.mimetype } }
+            ],
+        });
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
+app.post('/generate-from-audio', upload.single('audio'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Audio = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt ?? 'Tolong buatkan transkripsi dari audio ini.', type: 'text' },
+                { inlineData: { data: base64Audio, mimeType: req.file.mimetype } }
+            ],
+        });
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});

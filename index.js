8e9bab407781823ae56e291fba3aa1ff5ea3ae19
nurstd <w.nurhidayah07@gmail.com>
2026-06-20 15:09:46 +0700

    Inisiasi Create endpoint API using Gemini API

---
+++ index.js
@@ -0,0 +1,61 @@
+//Import semua library yang dibutuhkan
+import 'dotenv/config';
+import express from 'express';
+import multer from 'multer';
+import { GoogleGenAI } from '@google/genai';
+
+//Inisialisasi variable expres, multer, dan GoogleGenAI
+const app = express();
+const upload = multer();
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+//Inisialisasi variable untuk model yang akan digunakan
+const GEMINI_MODEL = 'gemini-2.5-flash';
+
+//Gunakan express.json() untuk parsing JSON
+app.use(express.json());
+
+//Menjalankan server pada port 3000
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server ready on http://localhost:${PORT}`));
+
+//endpoint untuk generate text
+app.post('/generate-text', async (req, res) => {
+    const { prompt } = req.body;
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        });
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
+//endpoint untuk generate gambar
+app.post('/generate-image', upload.single('image'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Image = req.file.buffer.toString("base64");
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt, type: "text" },
+                {
+                    inlineData: {
+                        mimeType: req.file.mimetype,
+                        data: base64Image
+                    }
+                }
+            ]
+        });
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+})
